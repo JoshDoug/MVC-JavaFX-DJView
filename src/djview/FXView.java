328845298dbcac349cc4f3178e88fee378db8202
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2017-01-08 22:45:11 +0000

    Add JavaFX View Class
    Adds JavaFX View Class (the default IntelliJ template) & ignore
    compiled java bytecode in out folder in gitignore.

---
+++ src/djview/FXView.java
@@ -0,0 +1,18 @@
+package djview;/**
+ * Created by joshstringfellow on 08/01/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FXView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
